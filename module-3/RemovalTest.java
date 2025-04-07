ba379f251baf70f9378b1b43d17fddd5a437b6dd
irene-cj <irenejaramillo10@gmail.com>
2025-04-06 23:44:55 -0500

    Update

--- module-3/RemovalTest.java
+++ module-3/RemovalTest.java
@@ -9,28 +9,33 @@ import java.util.Random;
  public class RemovalTest{
 
     public static void main(String[] args) {
-        
+        //This will create an arraylist with 50 random integers 1-20.
         ArrayList<Integer> ogList = generateRandomList(50, 1, 20);
 
         System.out.println("Original List: " + ogList);
-
+        //This arraylist will remove duplicates from the ogList.
         ArrayList<Integer> noDupList = removeDuplicates(ogList);
 
         System.out.println("No Duplicate List: " + noDupList);
     }
     
+    //This method will make a list with random integers between min-max.
     public static ArrayList<Integer> generateRandomList(int size, int min, int max) {
+        
         ArrayList<Integer> list = new ArrayList<>();
+        
+        //This will make a random object to make the random numbers.
         Random random = new Random();
         for (int i = 0; i < size; i++) {
             list.add(random.nextInt(max - min + 1) + min);
         }
+        //This will return the list.
         return list;
     }
 
 
     public static <E> ArrayList<E> removeDuplicates(ArrayList<E> list) {
-        // Using a HashSet to remove duplicates
+        //This HashSet is being used to remove duplicates.
         return new ArrayList<>(new HashSet<>(list));
     }
 }
